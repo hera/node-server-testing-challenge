dc0dda69fb0d69a8d1a25264ef146e96f1d53f6d
hera <a.heraimenka@gmail.com>
2020-07-23 12:22:52 -0700

    Function 'add'

--- api/posts/postsModel.js
+++ api/posts/postsModel.js
@@ -1,10 +1,27 @@
 const db = require("../../data/dbConfig");
 
 module.exports = {
-    getAll
+    getAll,
+    add
 };
+
+const TABLE_NAME = "post";
 
 
 function getAll () {
-    return db("post");
+    return db(TABLE_NAME);
+}
+
+
+function getById (id) {
+    return db(TABLE_NAME).where({id});
+}
+
+
+function add (postData) {
+    return db(TABLE_NAME)
+        .insert(postData, "id")
+        .then(ids => {
+            return getById(ids[0]);
+        })
 }
